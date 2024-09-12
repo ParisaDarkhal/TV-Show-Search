553c3196d1464f89314b8fb23977b5049da1d5ac
ParisaDarkhal <p.darkhal@gmail.com>
2024-09-12 19:20:10 -0400

    contentScript created

---
+++ contentScript.js
@@ -0,0 +1,11 @@
+// confirm("Hello form content script!");
+
+// const aTags = document.getElementsByTagName("a");
+// for (const tag of aTags) {
+//   tag.textContent = "Parisa";
+// }
+
+// const inputTags = document.getElementsByTagName("input");
+// for (const input of inputTags) {
+//   input.value = "Parisa";
+// }
